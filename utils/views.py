8cbbb8759279e6bfb32c6ce6bc68322ca51a2004
tpywao <tpywao@gmail.com>
2017-04-12 20:15:40 +0900

    Authed Views

--- utils/views.py
+++ utils/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.views.generic import View
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.utils.decorators import classonlymethod
 
-# Create your views here.
+
+class LoginRequiredViewMixin(object):
+    """
+    ログインが必要なViewのMixin
+    """
+    @classonlymethod
+    def as_view(cls, **kwargs):
+        view = super().as_view(**kwargs)
+        view = login_required(view)
+        return view
+
+
+class AuthedView(LoginRequiredViewMixin, View):
+    pass
+
+
+class AuthedTemplateView(LoginRequiredViewMixin, TemplateView):
+    pass
+
+
+class AuthedListView(LoginRequiredViewMixin, ListView):
+    pass
+
+
+class AuthedCreateView(LoginRequiredViewMixin, CreateView):
+    pass
+
+
+class AuthedUpdateView(LoginRequiredViewMixin, UpdateView):
+    pass
+
+
+class AuthedDeleteView(LoginRequiredViewMixin, DeleteView):
+    pass
+
+
+class AuthedDetailView(LoginRequiredViewMixin, DetailView):
+    pass

8a5c7d26101b385e0628ecf7d53604904d417908
tpywao <tpywao@gmail.com>
2017-04-15 03:59:48 +0900

    Add relationship between bookmark and tag

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+from tag.models import Tag
+
 
 class Bookmark(models.Model):
     # user = models.ForeignKey(User)
     name = models.CharField(max_length=140)
     url = models.URLField(max_length=2000)
-    # tag = models.ManyToManyField(Tag)
+    tag = models.ManyToManyField(Tag)
     # thumbnail = models.ImageField()
     # star = models.BooleanField()
     # archived = models.BooleanField()
